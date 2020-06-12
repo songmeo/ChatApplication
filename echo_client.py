422e21a72da553b086b4c49daeeeb4997dadc2a8
songmeo <shuong1012@gmail.com>
2020-06-12 14:25:52 +0300

    put into class

--- echo_client.py
+++ echo_client.py
@@ -1,25 +1,59 @@
 import sys, socket
-import lib
 
-HOST = sys.argv[-1] if len(sys.argv) > 1 else '127.0.0.1'
-PORT = lib.PORT
+class Client(object):
+	def __init__(self, host, port, sock):
+		self.HOST = host
+		self.PORT = port
+		self.SOCK = sock
+
+	def recv_msg(self):
+		data = bytearray()
+		msg = ''
+		while not msg:
+			recvd = SOCK.recv(4096)
+			if not recvd:
+				raise ConnectionError()
+			data = data + recvd
+			if b'\0' in recvd:
+				msg = data.rstrip(b'\0')
+		msg = msg.decode('utf-8')
+		print('Received echo: ' + msg)
+
+	def send_msg(self, msg):
+		msg += '\0'
+		data = msg.encode('utf-8')
+		SOCK.sendall(data)
+		print('Sent msg: {}'.format(msg))
+
+	def connect_socket(self):
+		SOCK.connect((HOST, PORT))
+		print('\nConnected to {}:{}'.format(HOST, PORT))
+
+	def close_socket(self):
+		SOCK.close()
+		print('Closed connection to server\n')
 
 if __name__ == '__main__':
+
+	HOST = sys.argv[-1] if len(sys.argv) > 1 else '127.0.0.1'
+	PORT = 4040
+	SOCK = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	client = Client(HOST, PORT, SOCK)
+
 	while True:
 		try:
-			sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-			sock.connect((HOST, PORT))
-			print('\nConnected to {}:{}'.format(HOST, PORT))
+			client.connect_socket()
+
 			print("Type message, enter to send, 'q' to quit")
 			msg = input()
-			if msg == 'q': break
-			lib.send_msg(sock, msg)
-			print('Sent msg: {}'.format(msg))
-			msg = lib.recv_msg(sock)
-			print('Received echo: ' +  msg)
+			if msg == 'q':
+				break
+
+			client.send_msg(msg)
+
 		except ConnectionError:
 			print('Socket error')
 			break
+
 		finally:
-			sock.close()
-			print('Closed connection to server\n')
+			client.close_socket()
--- echo_server.py
+++ echo_server.py
@@ -1,26 +1,58 @@
-import lib
+import socket
 
-HOST = '' #listening on all interfaces
-PORT = lib.PORT
+class Server(object):
+	def __init__(self, host, port, sock):
+		self.HOST = host
+		self.PORT = port
+		self.SOCK = sock
 
-def handle_client(sock, addr):
-	try:
-		msg = lib.recv_msg(sock)
-		print('{}: {}'.format(addr, msg))
-		lib.send_msg(sock, msg)
-	except (ConnectionError, BrokenPipeError):
-		print('Socket error')
-	finally:
-		print('Closed connection to {}'.format(addr))
-		sock.close()
+	def recv_msg(self, sock):
+		data = bytearray()
+		msg = ''
+		while not msg:
+			recvd = sock.recv(4096)
+			if not recvd:
+				raise ConnectionError()
+			data = data + recvd
+			if b'\0' in recvd:
+				msg = data.rstrip(b'\0')
+		msg = msg.decode('utf-8')
+		print('Received echo: ' + msg)
+		return msg
+
+	def send_msg(self, sock, msg):
+		msg += '\0'
+		data = msg.encode('utf-8')
+		sock.sendall(data)
+		print('Sent msg: {}'.format(msg))
+
+
+	def handle_client(self, client_sock, client_addr):
+		try:
+			msg = self.recv_msg(client_sock)
+			print('{}: {}'.format(client_addr, msg))
+			self.send_msg(client_sock, msg)
+		except (ConnectionError, BrokenPipeError):
+			print('Socket error')
+		finally:
+			print('Closed connection to {}'.format(client_addr))
+			client_sock.close()
 
 if __name__ == '__main__':
-	listen_sock = lib.create_listen_socket(HOST, PORT)
-	addr = listen_sock.getsockname()
+	HOST =  '' #listening on all interfaces
+	PORT = 4040
+
+	SOCK = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	SOCK.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	SOCK.bind((HOST, PORT))
+	SOCK.listen(100)
+
+	server = Server(HOST,PORT,SOCK)
+	addr = SOCK.getsockname()
 	print('Listening on {}'.format(addr))
 
 	while True:
-		client_sock, addr = listen_sock.accept()
-		print('Connection from {}'.format(addr))
-		handle_client(client_sock, addr)
-	
+		client_sock, client_addr = server.SOCK.accept()
+		print('Connection from {}'.format(client_addr))
+		server.handle_client(client_sock, client_addr)
+
